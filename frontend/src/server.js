0ea66c6aaaa36b67ece1a32c3a52644b43c193ee
Ashik9500 <ashikameen790@gmail.com>
2025-06-16 12:27:44 +0530

    Add database operations for Sleep Sessions

---
+++ frontend/src/server.js
@@ -0,0 +1,18 @@
+
+require('dotenv').config();
+const express = require('express');
+const connectDB = require("./Database/connection");
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+const sessionsRouter = require("./routes/sessions");
+
+app.use('/api', sessionsRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`)); 
